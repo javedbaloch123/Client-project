4ea88fa010ff28f2c85d0baf67a7ff718242a5f1
Javed <javedbaloch0091@gmail.com>
2025-08-15 14:50:56 +0500

    testing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layout.layout')
 
 @section('main')
-    <x-hero/>
-    <x-about/>
+    {{-- <x-hero/> --}}
+    {{-- <x-about/> --}}
     {{-- <x-stats/> --}}
     {{-- <x-skills/> --}}
-     <x-why-me/>
-    <x-portfolio/>
-    <x-free-guide/>
-    <x-services/>
+     {{-- <x-why-me/> --}}
+    {{-- <x-portfolio/> --}}
+    {{-- <x-free-guide/> --}}
+    {{-- <x-services/> --}}
     <x-testimonial/>
-    <x-contact/>
+    {{-- <x-contact/> --}}
 @endsection
